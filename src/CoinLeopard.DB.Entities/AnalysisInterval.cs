6207daeb8d9906a941ef33a4f0abd5c38a20b254
Terrance Jevon <terrance@hoa.co.za>
2022-11-29 20:45:57 +0200

    Chore: Add last market price

--- src/CoinLeopard.DB.Entities/AnalysisInterval.cs
+++ src/CoinLeopard.DB.Entities/AnalysisInterval.cs
@@ -7,6 +7,7 @@ public class AnalysisInterval
     public decimal Inclination { get; set; }
     public decimal Low { get; set; }
     public decimal High { get; set; }
+    public decimal LastMarketPrice { get; set; }
     public decimal InclinationDirectionFactor { get; set; }
     public DateTime Start { get; set; }
     public DateTime End { get; set; }
---
+++ src/CoinLeopard.DB.Migrations.PostgreSQL/Migrations/20221119101227_LastMarkPrice.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoinLeopard.DB.Migrations.PostgreSQL.Migrations
+{
+    public partial class LastMarkPrice : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "LastMarketPrice",
+                table: "Analyses",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastMarketPrice",
+                table: "Analyses");
+        }
+    }
+}

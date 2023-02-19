f71a5f7b6ae0797ccb954082cf6eb482d22fd40e
Terrance Jevon <terrance@hoa.co.za>
2023-02-19 21:06:14 +0200

    Chore: Positions closed attributes

--- src/CoinLeopard.DB.Entities/FuturesPosition.cs
+++ src/CoinLeopard.DB.Entities/FuturesPosition.cs
@@ -12,6 +12,9 @@ public class FuturesPosition
 	public decimal StopPrice { get; set; }
 	public long? StopLossOrderId { get; set; }
 	public long? TakeProfitOrderId { get; set; }
+	public bool Closed { get; set; }
+	public DateTime DateOpened { get; set; }
+	public DateTime? DateClosed { get; set; }
 	public FuturesOrderSide OrderSide { get; set; }
 	public FuturesPositionSide PositionSide { get; set; }
 	public FuturesPositionType Type { get; set; }
---
+++ src/CoinLeopard.DB.Migrations.PostgreSQL/Migrations/20230219190558_PositionsClosedAttributes.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoinLeopard.DB.Migrations.PostgreSQL.Migrations
+{
+    public partial class PositionsClosedAttributes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Closed",
+                table: "FuturesPositions",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateClosed",
+                table: "FuturesPositions",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOpened",
+                table: "FuturesPositions",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Closed",
+                table: "FuturesPositions");
+
+            migrationBuilder.DropColumn(
+                name: "DateClosed",
+                table: "FuturesPositions");
+
+            migrationBuilder.DropColumn(
+                name: "DateOpened",
+                table: "FuturesPositions");
+        }
+    }
+}

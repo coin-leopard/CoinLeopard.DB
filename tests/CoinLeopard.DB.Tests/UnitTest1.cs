7e696640802bd14921d9ff786e9aee10c9cd37f2
terrance <terrancejevon@gmail.com>
2025-06-12 13:36:55 +0200

    Chore: Upgrade packages

--- tests/CoinLeopard.DB.Tests/UnitTest1.cs
+++ tests/CoinLeopard.DB.Tests/UnitTest1.cs
@@ -2,9 +2,9 @@ namespace CoinLeopard.DB.Tests;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
-    {
-        Console.WriteLine("Do a test");
-    }
+	[Fact]
+	public void Test1()
+	{
+		Console.WriteLine("Do a test");
+	}
 }

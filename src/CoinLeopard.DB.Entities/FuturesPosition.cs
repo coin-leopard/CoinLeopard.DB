8bbd5b4276a14ff7eb19c6c47670dbd8c912807f
Terrance Jevon <terrancejevon@gmail.com>
2025-06-24 15:59:30 +0200

    Update FuturesPosition to have orderId

--- src/CoinLeopard.DB.Entities/FuturesPosition.cs
+++ src/CoinLeopard.DB.Entities/FuturesPosition.cs
@@ -7,7 +7,7 @@ public class FuturesPosition
 	public Guid Id { get; set; }
 	public string Symbol { get; set; } = null!;
 	public decimal EntryPrice { get; set; }
-    public decimal Quantity { get; set; }
+	public decimal Quantity { get; set; }
 	public decimal TakeProfitPrice { get; set; }
 	public decimal StopPrice { get; set; }
 	public long? StopLossOrderId { get; set; }
@@ -19,6 +19,7 @@ public class FuturesPosition
 	public FuturesPositionSide PositionSide { get; set; }
 	public FuturesPositionType Type { get; set; }
 	public string ClientOrderId { get; set; } = null!;
+	public long? OrderId { get; set; }
 	public decimal? PNL { get; set; }
 	public virtual FuturesSymbol? FuturesSymbol { get; set; }
 	public string? AnalysisSnapshot { get; set; }
---
+++ src/CoinLeopard.DB.Migrations.PostgreSQL/Migrations/20250624135846_FuturesPosition_OrderId.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoinLeopard.DB.Migrations.PostgreSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class FuturesPosition_OrderId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "OrderId",
+                table: "FuturesPositions",
+                type: "bigint",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrderId",
+                table: "FuturesPositions");
+        }
+    }
+}

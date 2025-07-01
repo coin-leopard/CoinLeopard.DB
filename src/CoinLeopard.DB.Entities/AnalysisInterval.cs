e35bb81029b32762ccd1a89521f4d1c2354c9c77
Terrance Jevon <terrancejevon@gmail.com>
2025-07-01 15:40:26 +0200

    Price Action

--- src/CoinLeopard.DB.Entities/AnalysisInterval.cs
+++ src/CoinLeopard.DB.Entities/AnalysisInterval.cs
@@ -2,14 +2,15 @@
 
 public class AnalysisInterval
 {
-    public Guid Id { get; set; }
-    public string Symbol { get; set; } = null!;
-    public decimal Inclination { get; set; }
-    public decimal Low { get; set; }
-    public decimal High { get; set; }
-    public decimal LastMarketPrice { get; set; }
-    public decimal InclinationDirectionFactor { get; set; }
-    public DateTime Start { get; set; }
-    public DateTime End { get; set; }
-    public FuturesSymbol? FuturesSymbol { get; set; }
+	public Guid Id { get; set; }
+	public string Symbol { get; set; } = null!;
+	public decimal Inclination { get; set; }
+	public decimal Low { get; set; }
+	public decimal High { get; set; }
+	public decimal LastMarketPrice { get; set; }
+	public decimal InclinationDirectionFactor { get; set; }
+	public decimal PercentagePriceAction { get; set; }
+	public DateTime Start { get; set; }
+	public DateTime End { get; set; }
+	public FuturesSymbol? FuturesSymbol { get; set; }
 }
---
+++ src/CoinLeopard.DB.Migrations.PostgreSQL/Migrations/20250701134013_PriceAction.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoinLeopard.DB.Migrations.PostgreSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class PriceAction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "PercentagePriceAction",
+                table: "Analyses",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PercentagePriceAction",
+                table: "Analyses");
+        }
+    }
+}

145397181fee29dd668996a648fde9e76bf912d1
Terrance Jevon <terrancejevon@gmail.com>
2025-08-23 00:01:37 +0200

    Chore: Cleanup

---
+++ src/CoinLeopard.DB.Migrations.PostgreSQL/Migrations/20250822220112_JustToBeClean.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoinLeopard.DB.Migrations.PostgreSQL.Migrations
+{
+	/// <inheritdoc />
+	public partial class JustToBeClean : Migration
+	{
+		/// <inheritdoc />
+		protected override void Up(MigrationBuilder migrationBuilder) { }
+
+		/// <inheritdoc />
+		protected override void Down(MigrationBuilder migrationBuilder) { }
+	}
+}

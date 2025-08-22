e6270c22e065f385e148aa4b8dc849c2c3619c7f
Terrance Jevon <terrancejevon@gmail.com>
2025-08-23 00:11:35 +0200

    Fix

--- src/CoinLeopard.DB.Migrations.PostgreSQL/Migrations/20250822215713_ApparentlyNecessary.cs
+++ src/CoinLeopard.DB.Migrations.PostgreSQL/Migrations/20250822215713_ApparentlyNecessary.cs
@@ -8,40 +8,9 @@ namespace CoinLeopard.DB.Migrations.PostgreSQL.Migrations
 	public partial class ApparentlyNecessary : Migration
 	{
 		/// <inheritdoc />
-		protected override void Up(MigrationBuilder migrationBuilder)
-		{
-			migrationBuilder.DropForeignKey(name: "FK_Heuristics_FuturesSymbols_FuturesSymbolSymbol", table: "Heuristics");
-
-			migrationBuilder.DropColumn(name: "FuturesSymbolSymbol", table: "Heuristics");
-
-			migrationBuilder.CreateIndex(name: "IX_Heuristics_Symbol_Name", table: "Heuristics", columns: new[] { "Symbol", "Name" });
-
-			migrationBuilder.AddForeignKey(
-				name: "FK_Heuristics_FuturesSymbols_Symbol",
-				table: "Heuristics",
-				column: "Symbol",
-				principalTable: "FuturesSymbols",
-				principalColumn: "Symbol",
-				onDelete: ReferentialAction.Cascade
-			);
-		}
+		protected override void Up(MigrationBuilder migrationBuilder) { }
 
 		/// <inheritdoc />
-		protected override void Down(MigrationBuilder migrationBuilder)
-		{
-			migrationBuilder.DropForeignKey(name: "FK_Heuristics_FuturesSymbols_Symbol", table: "Heuristics");
-
-			migrationBuilder.DropIndex(name: "IX_Heuristics_Symbol_Name", table: "Heuristics");
-
-			migrationBuilder.AddColumn<string>(name: "FuturesSymbolSymbol", table: "Heuristics", type: "text", nullable: true);
-
-			migrationBuilder.AddForeignKey(
-				name: "FK_Heuristics_FuturesSymbols_FuturesSymbolSymbol",
-				table: "Heuristics",
-				column: "FuturesSymbolSymbol",
-				principalTable: "FuturesSymbols",
-				principalColumn: "Symbol"
-			);
-		}
+		protected override void Down(MigrationBuilder migrationBuilder) { }
 	}
 }
